de61903f3bf09c1cf27982a85aa7242e4ff1a8d2
Bilal Khatri <engrbilalfarooque@gmail.com>
2023-09-26 01:47:32 +0500

    section4 done

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-luxury 
-256 Cars
-Sports
-560 Cars
-Suv
-295 Cars
-Monthly
-175 Cars
-High Price
-560 Cars
-Car with Driver
-768 Cars
-Electric
-560 Cars
-Convertible
-265 Cars
+document.addEventListener("DOMContentLoaded", function() {
+    const cardCarousel = document.querySelector(".card-carousel");
+    const prevButton = document.querySelector(".prev-button");
+    const nextButton = document.querySelector(".next-button");
+
+    let currentIndex = 0;
+
+    // Function to move to the previous card
+    function showPreviousSlide() {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = cardCarousel.childElementCount - 1;
+        }
+        updateCarousel();
+    }
+
+    // Function to move to the next card
+    function showNextSlide() {
+        currentIndex++;
+        if (currentIndex >= cardCarousel.childElementCount) {
+            currentIndex = 0;
+        }
+        updateCarousel();
+    }
+
+    // Function to update the carousel's position
+    function updateCarousel() {
+        const translateValue = -currentIndex * 100;
+        cardCarousel.style.transform = `translateX(${translateValue}%)`;
+    }
+
+    // Attach event listeners to the previous and next buttons
+    prevButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+});
